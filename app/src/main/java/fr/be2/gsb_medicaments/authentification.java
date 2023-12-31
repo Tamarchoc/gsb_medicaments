effdfeae8b857fa56f8941221f1d97dd9e75acec
tamar chocron <tamarchoc@gmail.com>
2023-12-31 14:34:56 +0100

    Initial commit

---
+++ app/src/main/java/fr/be2/gsb_medicaments/authentification.java
@@ -0,0 +1,53 @@
+package fr.be2.gsb_medicaments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.security.SecureRandom;
+
+public class authentification extends AppCompatActivity {
+    private EditText CodeV,myKey;
+    private Button btnValiderCodeV,btnValidercle;
+    LinearLayout LayoutCle ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+        CodeV = findViewById(R.id.codeV);
+        myKey = findViewById(R.id.myKey);
+       LayoutCle = findViewById(R.id.layoutCle);
+       LayoutCle.setVisibility(View.INVISIBLE);
+
+
+    }
+    public void AfficheLayout(View v){
+        LayoutCle.setVisibility(View.INVISIBLE);
+        String myRandomKey = genererChaineAleatoire( 5);
+        Log.d("APPLI","myKey="+myRandomKey);
+
+    }
+    private String genererChaineAleatoire(int longueur) {
+        String caracteresPermis = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder chaineAleatoire = new StringBuilder();
+
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < longueur; i++) {
+            int index = random.nextInt(caracteresPermis.length());
+            char caractereAleatoire = caracteresPermis.charAt(index);
+            chaineAleatoire.append(caractereAleatoire);
+        }
+
+        return chaineAleatoire.toString();
+    }
+
+
+}
